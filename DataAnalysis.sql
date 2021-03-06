21c96c6763aea88976fe5dd3026ea27828760a39
Anthony Njuguna <tonju2@yahoo.com>
2021-03-06 02:07:01 -0600

    5th version

--- DataAnalysis.sql
+++ DataAnalysis.sql
@@ -45,3 +45,48 @@ INNER JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
 INNER JOIN departments ON departments.dept_no=dept_emp.dept_no
 order by emp_no;
 
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" 
+-- and last names begin with "B."
+
+SELECT first_name, last_name, sex FROM employees
+WHERE first_name = 'Hercules' AND last_name like 'B%';
+
+-- 6. List all employees in the Sales department, including their employee number, 
+-- last name, first name, and department name.
+
+SELECT 
+employees.emp_no, 
+employees.last_name, 
+employees.first_name,
+departments.dept_name
+FROM employees 
+LEFT JOIN dept_emp 
+ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments 
+ON departments.dept_no=dept_emp.dept_no
+WHERE departments.dept_name='Sales';
+
+-- 7. List all employees in the Sales and Development departments, including their 
+-- employee number, last name, first name, and department name.
+
+SELECT 
+employees.emp_no, 
+employees.last_name, 
+employees.first_name,
+departments.dept_name
+FROM employees 
+LEFT JOIN dept_emp 
+ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments 
+ON departments.dept_no=dept_emp.dept_no
+WHERE departments.dept_name in ('Sales', 'Development');
+
+-- 8. In descending order, list the frequency count of employee last names, 
+-- i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(*) AS freq_count
+FROM employees
+GROUP BY last_name
+ORDER BY freq_count DESC;
+
+
